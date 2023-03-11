58f4a8b18727a17624c13dd76cd254877f1d069c
Marco Lecheler <marco.lecheler@mercedes-benz.com>
2023-03-11 21:12:58 +0100

    fix: do not return error to HTTP request
    Error will be returned to log

--- pkg/backend/api/generic/list.go
+++ pkg/backend/api/generic/list.go
@@ -1,14 +1,21 @@
 package generic
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
 
 func HandleListRequest(c *gin.Context, getDataFunc func() (interface{}, error)) {
 	data, err := getDataFunc()
 	if err != nil {
 		c.IndentedJSON(500, gin.H{
 			"message": "Internal server error",
-			"error":   err.Error(),
+			"error":   "Error handling list request",
 		})
+
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("Error handling list request")
 		return
 	}
 
