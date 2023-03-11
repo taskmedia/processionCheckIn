b62b63e0a64a12748e441fa63bc9fa5081b90397
Marco Lecheler <marco.lecheler@mercedes-benz.com>
2023-03-11 22:10:26 +0100

    feat: improve db maintain error handling

--- pkg/backend/db/file.go
+++ pkg/backend/db/file.go
@@ -1,30 +1,44 @@
 package db
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 
 	_ "github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
 )
 
-func executePsqlFile(filename string) {
-	// Read SQL commands from file
+func executePsqlFile(filename string) error {
+	// Open SQL file
 	file, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("Error opening SQL file")
+
+		return err
 	}
 	defer file.Close()
 
-	// Execute SQL commands
+	// Read SQL file
 	commands, err := ioutil.ReadAll(file)
 	if err != nil {
-		panic(err)
-	}
-	_, err = DbConn.Exec(string(commands))
-	if err != nil {
-		panic(err)
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("Error reading SQL file")
+
+		return err
 	}
 
-	fmt.Println("SQL commands executed successfully.")
+	// Execute SQL file
+	_, err = DbConn.Exec(string(commands))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("Error executing SQL file")
+
+		return err
+	}
+
+	return nil
 }
--- pkg/backend/db/maintain.go
+++ pkg/backend/db/maintain.go
@@ -1,20 +1,51 @@
 package db
 
-func dropDb() {
-	executePsqlFile("./psql/drop.psql")
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+func dropDb() error {
+	err := executePsqlFile("./psql/drop.psql")
+	if err != nil {
+
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("Error dropping database")
+
+		return err
+	}
+
+	return nil
 }
 
-func InitDb() {
-	executePsqlFile("./psql/init-table.psql")
+func InitDb() error {
+	err := executePsqlFile("./psql/init-table.psql")
+	if err != nil {
+
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("Error initializing database")
+
+		return err
+	}
+
+	return nil
 }
 
-func ResetDb() {
-	dropDb()
-	InitDb()
+func ResetDb() error {
+	err := dropDb()
+	if err != nil {
+		return err
+	}
+
+	return InitDb()
 }
 
-func ResetDbExampledata() {
-	ResetDb()
+func ResetDbExampledata() error {
+	err := ResetDb()
+	if err != nil {
+		return err
+	}
 
-	executePsqlFile("./psql/exampledata.psql")
+	return executePsqlFile("./psql/exampledata.psql")
 }
