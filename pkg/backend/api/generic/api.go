68e0ff1329bb06b06332bb89555226a216a53120
Marco Lecheler <marco.lecheler@mercedes-benz.com>
2023-03-11 22:01:11 +0100

    chore: rename routers parameter addRouter

--- pkg/backend/api/generic/api.go
+++ pkg/backend/api/generic/api.go
@@ -2,7 +2,7 @@ package generic
 
 import "github.com/gin-gonic/gin"
 
-func HandleGroupRouters(r *gin.RouterGroup, path string, routers func(router *gin.RouterGroup)) {
+func HandleGroupRouters(r *gin.RouterGroup, path string, addRouter func(router *gin.RouterGroup)) {
 	rg := r.Group(path)
-	routers(rg)
+	addRouter(rg)
 }
